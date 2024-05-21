72419b99d078fae8ba90a16c8fc6659d86df42a4
René Barth <83969798+uffrc@users.noreply.github.com>
2024-05-21 15:55:03 +0200

    add combiner.py

---
+++ combiner.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import PyPDF2
+import sys
+
+def combine_pdfs(output_path, *input_paths):
+    pdf_writer = PyPDF2.PdfWriter()
+    
+    for path in input_paths:
+        pdf_reader = PyPDF2.PdfReader(path)
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
+            pdf_writer.add_page(page)
+    
+    with open(output_path, 'wb') as out:
+        pdf_writer.write(out)
+
+if __name__ == "__main__":
+    output_path = sys.argv[1]
+    input_paths = sys.argv[2:]
+    combine_pdfs(output_path, *input_paths)
+
